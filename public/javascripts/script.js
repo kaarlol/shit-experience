a6f83c9c493c8dbb2d11550fb447abcbd5ab2d41
Kaarlo Laite <kaarlo.laite@nitro.fi>
2019-05-17 15:17:47 +0300

    Made mute button moving

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,21 +1,18 @@
 $(document).ready(function() {
   var audio = document.getElementById("rickRoll");
-  moveAudioControl();
   $("#audioControl").click(function() {
     if (audio.muted == true) {
       audio.muted = false;
       $("#audioControl").html("MUTE");
-      moveAudioControl();
     } else {
       audio.muted = true;
       $("#audioControl").html("UNMUTE");
-      moveAudioControl();
     }
   });
 });
 
-function moveAudioControl() {
+setInterval(function() {
   var audioControlTop = window.innerHeight * Math.random();
   var audioControlLeft = window.innerWidth * Math.random();
   $("#audioControl").css({ top: audioControlTop, left: audioControlLeft });
-}
+}, 1500);
